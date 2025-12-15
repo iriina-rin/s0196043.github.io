386169df8509373abf8a7c22fd90e9d559123086
iriina-rin <irun4ikug@gmail.com>
2025-12-15 20:59:43 +0300

    newjavascript.js

--- файл8/newjavascript.js
+++ файл8/newjavascript.js
@@ -59,44 +59,35 @@ document.addEventListener('DOMContentLoaded', function() {
         localStorage.setItem('formData', JSON.stringify(formData));
     });
 
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
-        
-        const checkbox = document.getElementById('checkbox');
-        if (!checkbox.checked) {
-            alert('Без согласия политики обработки персональных данныз сообщение не может быть отправлено');
-            return;
+    form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const checkbox = document.getElementById('checkbox');
+    if (!checkbox.checked) {
+        alert('Без согласия политики обработки персональных данных сообщение не может быть отправлено');
+        return;
+    }
+
+    const formData = new FormData(form);
+
+    fetch('https://formcarry.com/s/YhFifbGcXzg', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.code === 200) {
+            alert('Сообщение успешно отправлено');
+            localStorage.removeItem('formData');
+            form.reset();
+            closePopup();
+        } else {
+            alert('Ошибка отправки');
         }
-        
-        const formData = getFormData();
-
-        fetch('https://formcarry.com/s/YhFifbGcXzg', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(formData)
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.code === 200) {
-                alert('Сообщение успешно отправлено');
-                localStorage.removeItem('formData');
-                closePopup();
-
-                document.getElementById('name').value = '';
-                document.getElementById('email').value = '';
-                document.getElementById('tel').value = '';
-                document.getElementById('organization').value = '';
-                document.getElementById('message').value = '';
-                document.getElementById('checkbox').checked = false;
-            } else {
-                alert('Ошибка отправки');
-            }
-        })
-        .catch(error => {
-            alert('Ошибка сети');
-        });
+    })
+    .catch(() => {
+        alert('Ошибка сети');
     });
+});
+
 });
