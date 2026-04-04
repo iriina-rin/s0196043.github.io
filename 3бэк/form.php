6f39e27f999d9fb25795136508b6d6e686e682a0
iriina-rin <irun4ikug@gmail.com>
2026-04-04 23:21:52 +0300

    Update form.php

--- 3бэк/form.php
+++ 3бэк/form.php
@@ -1,48 +1,13 @@
+
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+header('Content-Type: text/html; charset=UTF-8');
 
-$dsn = 'mysql:host=localhost;dbname=u82815';
-$user = 'u82815';
-$pass = '3582298';
-
-$db = new PDO($dsn,$user,$pass);
-
-$name = $_POST['name'];
-$phone = $_POST['phone'];
-$email = $_POST['email'];
-$birthdate = $_POST['birthdate'];
-$gender = $_POST['gender'];
-$bio = $_POST['bio'];
-
-$contract = isset($_POST['contract']) ? 1 : 0;
-
-$languages = $_POST['languages'];
-
-$stmt = $db->prepare("INSERT INTO application
-(name,phone,email,birthdate,gender,biography,contract)
-VALUES (?,?,?,?,?,?,?)");
-
-$stmt->execute([
-$name,
-$phone,
-$email,
-$birthdate,
-$gender,
-$bio,
-$contract
-]);
-
-$app_id = $db->lastInsertId();
-
-foreach($languages as $lang){
-
-$stmt = $db->prepare("INSERT INTO application_languages
-(application_id,language_id)
-VALUES (?,?)");
-
-$stmt->execute([$app_id,$lang]);
-
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    if (!empty($_GET['save'])) {
+        print('Спасибо, результаты сохранены.');
+    }
+    include('index.php');
+    exit();
 }
-
-echo "Данные сохранены";
-
-?>
