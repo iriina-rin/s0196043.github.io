046bf822dc8e444ce1a114dbf280b34a25e03a86
iriina-rin <irun4ikug@gmail.com>
2026-04-26 12:02:46 +0300

    Update index.php

--- 5бэк/index.php
+++ 5бэк/index.php
@@ -30,7 +30,7 @@ function setErrorCookie($name, $message) {
 }
 
 // Подключение к БД
-$db = new PDO("mysql:host=localhost;dbname=u68775", 'u68775', '7631071', [
+$db = new PDO("mysql:host=localhost;dbname=u82815", 'u82815', '3583398', [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 ]);
 // Очистка ошибок при первом заходе
